32d9d2245e22e17f2a07f32af547d13d9dd532c1
Charvik <Charvik@Charvik-HP>
2018-11-08 18:23:26 +0530

    Fail

--- SamplePassFail/src/PackageNew/SampleClass.java
+++ SamplePassFail/src/PackageNew/SampleClass.java
@@ -4,7 +4,8 @@ public class SampleClass {
 	
 	 public static void main(String[] args) {
 	
-	String var1= "Pass";
+	//String var1= "Pass";
+	String var1= "Fail";
 	System.out.println("Result = " + var1);
 	
 	 }
